aed8dca474eb4265e5506803aaf20b161ee90311
Zad-Walid <zadwalid06@gmail.com>
2023-02-23 22:02:33 +0200

    BackEnd

---
+++ Postgres Database/TASK-2/5.sql
@@ -0,0 +1,6 @@
+/*https://www.hackerrank.com/challenges/name-of-employees/problem?isFullScreen=true*/
+
+/*
+Enter your query here.
+*/
+SELECT name FROM EMPLOYEE ORDER BY name ASC;
---
+++ Postgres Database/TASK-3/05.sql
@@ -0,0 +1,5 @@
+/*https://www.hackerrank.com/challenges/salary-of-employees/problem?isFullScreen=true*/
+
+/*
+Enter your query here.
+*/SELECT name FROM Employee WHERE Salary > 2000 AND months <10 ORDER BY employee_id ASC;
