d595784fa85748de3a7c2ebb52af3695476f3174
Zad-Walid <104693628+Zad-Walid@users.noreply.github.com>
2023-03-04 21:32:26 +0200

    BackEnd

---
+++ PHP/Task-5/08.php
@@ -0,0 +1,6 @@
+
+<?php
+echo nl2br ("We \n Love \n Elzero \n Web \n School");
+
+
+?>
---
+++ PHP/Task-5/12.php
@@ -0,0 +1,15 @@
+<?php
+
+$arr = ["FrontEnd" => ["html" ,"css"],
+"JS" =>["Vuejs" => ["v2","v3"] ,0=>"Reacctjs" ,1=>"Svelte"],
+"BackEnd"=> [0=>"PHP",1=>"MYSQL",2=>"Security"],
+0=>"git",1=>"Github",
+"testing"=>[0=>"Unit Testing", 1=>"End to end" ,2=> "Integeration"],
+
+];
+
+echo "<pre>";
+print_r($arr);
+echo "</pre>";
+?>
+
